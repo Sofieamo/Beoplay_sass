27cba1c4fbef81ef019cbcc4b0f0cf8b5a06b1a2
Sofieamo <sofieamaliem@gmail.com>
2017-02-20 11:08:17 +0100

    moved files
    Moved files

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {                         
+ $("#modal-box").css({"display":"block"});
+ $("#mybutton").click(function(){
+        $("#modal-box").hide();
+    });
+});
+
+ 
